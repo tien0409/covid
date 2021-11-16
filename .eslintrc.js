3ae42961e03447d5a5ae752c8eabcb9cb92c1c80
Le Anh Tien <tienla@flextech.vn>
2021-11-16 20:52:53 +0700

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "indent": ["error", 4],
+        "max-len": ["warn", 260]
+    }
+};
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res)=> {
+    res.send("hello world");
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
