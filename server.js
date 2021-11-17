76ff1b5848ad39ffeba2bac427594bea03faf774
Le Anh Tien <tienla@flextech.vn>
2021-11-17 21:00:56 +0700

    feat: add morgan for test in development

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 8080;
@@ -7,5 +8,8 @@ const PORT = 8080;
 // import utils
 const conn = require('./utils/db');
 conn();
+
+// middlewares
+app.use(morgan('dev'));
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
